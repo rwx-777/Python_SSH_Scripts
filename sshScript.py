b9a655b89a9b76c38179e9c93e6a10ebb1609164
rwx-777 <joshuajared007@gmail.com>
2019-11-15 15:09:20 +0100

    Added Exceptions made it fool proof i hope

--- sshScript.py
+++ sshScript.py
@@ -5,27 +5,28 @@
 import sys
 import paramiko
 
-host = "10.1.1.154" #change this to the IP of the Machine you want to SSH into
+host = "IP" #change this to the IP of the Machine you want to SSH into
 user = input("Username: ") 
 pswd = input("Password: ")
 
 client = paramiko.SSHClient()
-print("Calling paramiko")
 client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-
+print("Connecting...")
 
 try:
-    command = input("Command: ")
-    while(command != "exit"):
-		client.connect(hostname=host,username=user,password=pswd)
-		#command = input("Command: ")
-		client.invoke_shell()
-		stdin, stdout, stderr = client.exec_command(command)
-		print(stdout.read())
-
+    client.connect(hostname=host,username=user,password=pswd)
 except Exception as e:
-	print("Connection Failed")
-	print(e)
+    print(e)
+else:
+    i = 1
 
-finally:
-	client.close()
+    while (i == 1):
+        command = input("Command: ")
+        client.invoke_shell()
+        stdin,stdout,stderr = client.exec_command(command)
+        print(stdout.read())
+
+        if command == "exit":
+            print("Goodbye " + user)
+            client.close()
+            break
